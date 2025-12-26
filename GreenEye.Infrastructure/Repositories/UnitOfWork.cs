66c954e4337554714c757b2eed346aed773418ba
Mahmoudssanad <sanadmahmoud968@gmail.com>
2025-12-27 01:21:38 +0200

    Add: unit of work and repo pattern setup

---
+++ GreenEye.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using GreenEye.Domain.IRepositories;
+using GreenEye.Infrastructure.Data;
+
+namespace GreenEye.Infrastructure.Repositories
+{
+    public class UnitOfWork(AppDbContext _context) : IUnitOfWrok
+    {
+        public async Task CompleteAsync() => await _context.SaveChangesAsync();
+        
+        public void Dispose() => _context.Dispose();
+       
+    }
+}
